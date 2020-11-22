6577fdcb5c024d7c00719146a1f3790acb23269f
Olivier De Meulder <olivier.demeulder@nytimes.com>
2020-11-22 16:57:33 -0500

    init

---
+++ generate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+FILE="proxy.xml"
+
+/bin/cat <<EOM >$FILE
+<proxy>
+  <head>
+    <meta>Some general info</meta>
+  </head>
+  <endpoints>
+    <endpoint>
+      <path>/entitlements</path>
+      <policy>mypolicy</policy>
+      <method>GET</method>
+    </endpoint>
+    <endpoint>
+      <path>/entitlements</path>
+      <policy>custom_policy</policy>
+      <method>POST</method>
+    </endpoint>
+    <endpoint>
+      <path>/subscriptions</path>
+      <policy>other_policy</policy>
+      <method>GET</method>
+    </endpoint>
+  </endpoints>
+</proxy>
+EOM
+
